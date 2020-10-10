30b207607a05eef7b7b1dd5c0624fabb129afbd6
lalit17 <lalitrajput069@gmail.com>
2020-10-10 16:16:41 +0530

    Initial commit

--- app/src/main/java/com/example/myfavritelist/MainActivity.java
+++ app/src/main/java/com/example/myfavritelist/MainActivity.java
@@ -1,14 +1,17 @@
 package com.example.myfavritelist;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        EditText editText = new EditText(this);
+        AlertDialog.Builder alertDialogBox = new AlertDialog.Builder(this);
+        alertDialogBox.setTitle("Enter Category Name").setView(editText).setPositiveButton("Done", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
