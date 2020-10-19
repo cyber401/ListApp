ff7634cad694735d4416408b8145bc8b55b87752
lalit17 <lalitrajput069@gmail.com>
2020-10-20 00:16:02 +0530

    1st step clear

--- app/src/main/java/com/example/myfavritelist/MainActivity.java
+++ app/src/main/java/com/example/myfavritelist/MainActivity.java
@@ -12,7 +12,6 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-
 import android.text.InputType;
 import android.util.Log;
 import android.view.View;
